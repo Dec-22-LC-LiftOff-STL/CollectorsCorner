0ed135d93a5c1d339f3f345024063fc0f8043aed
Rich Ives <rjsives89@gmail.com>
2022-12-13 21:07:00 -0600

    Adding Spring Boot Files (initialized using the same configurations that Nick suggested on his Dec 8th Slack message).

---
+++ demo/src/test/java/com/collectorscorner/demo/CollectorsCornerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.collectorscorner.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CollectorsCornerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
