9217e7ffda0a5e42cc19138096252eb178c22536
Nick Hoefle <nickhoefle@gmail.com>
2022-12-14 18:08:24 -0600

    Added Files for Movie API List. Worked when tested.

---
+++ src/main/java/com/collectorscorner/demo/data/MovieRepository.java
@@ -0,0 +1,12 @@
+package com.collectorscorner.demo.data;
+
+import com.collectorscorner.demo.models.Movie;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MovieRepository extends CrudRepository<Movie, Integer> {
+
+
+
+}
---
+++ src/main/java/com/collectorscorner/demo/models/Movie.java
@@ -0,0 +1,117 @@
+package com.collectorscorner.demo.models;
+
+import jakarta.persistence.Entity;
+import org.springframework.stereotype.Component;
+
+@Entity
+@Component
+public class Movie extends AbstractEntity {
+
+    private String title;
+
+    private int year;
+
+    private String director;
+
+    private String genre;
+
+    private String genre2;
+
+    private String genre3;
+
+    private String synopsis;
+
+    private int userID;
+
+    public Movie() {}
+
+    public Movie(String title, int year, String director, String genre, String genre2, String genre3, String synopsis, int userID) {
+        this.title = title;
+        this.year = year;
+        this.director = director;
+        this.genre = genre;
+        this.genre2 = genre2;
+        this.genre3 = genre3;
+        this.synopsis = synopsis;
+        this.userID = userID;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public String getDirector() {
+        return director;
+    }
+
+    public void setDirector(String director) {
+        this.director = director;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public String getGenre2() {
+        return genre2;
+    }
+
+    public void setGenre2(String genre2) {
+        this.genre2 = genre2;
+    }
+
+    public String getGenre3() {
+        return genre3;
+    }
+
+    public void setGenre3(String genre3) {
+        this.genre3 = genre3;
+    }
+
+    public String getSynopsis() {
+        return synopsis;
+    }
+
+    public void setSynopsis(String synopsis) {
+        this.synopsis = synopsis;
+    }
+
+    public int getUserID() {
+        return userID;
+    }
+
+    public void setUserID(int userID) {
+        this.userID = userID;
+    }
+
+    @Override
+    public String toString() {
+        return "Movie{" +
+                "title='" + title + '\'' +
+                ", year=" + year +
+                ", director='" + director + '\'' +
+                ", genre='" + genre + '\'' +
+                ", genre2='" + genre2 + '\'' +
+                ", genre3='" + genre3 + '\'' +
+                ", synopsis='" + synopsis + '\'' +
+                ", userID=" + userID +
+                '}';
+    }
+}
+
