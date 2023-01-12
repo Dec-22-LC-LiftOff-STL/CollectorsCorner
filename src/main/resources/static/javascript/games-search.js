a7e01cec84292cfc80e97716249b7b9d8a85eade
Nick Hoefle <nickhoefle@gmail.com>
2023-01-12 14:55:18 -0600

    Game title search working and rough unvalidated results table working.

--- src/main/resources/static/javascript/games-search.js
+++ src/main/resources/static/javascript/games-search.js
@@ -9,15 +9,15 @@ function searchTitle() {
     let url = urlBeginning + searchTerm + urlEnding;
     buildHTMLResultsTable(url);
     //Only display the "Show Filters" button after someone searches
-    document.getElementById("showFiltersButton").style.display = "block";
+//    document.getElementById("showFiltersButton").style.display = "block";
     //Hides Submit button after a search and has user use New Search button for a new search so filters are not mixed up
-    document.getElementById("searchTermAndType").style.display = "none";
+//    document.getElementById("searchTermAndType").style.display = "none";
 }
 
 function buildHTMLResultsTable(url) {
     fetch(url).then(function(response) {
     response.json().then(function(json) {
-    const arrayOfGameObjects = json.results;
+    const arrayOfGameObjects = json.games;
     const resultsTable = document.getElementById("resultsTable"); //See search.html template
     let tableBeginning = `
     <table>
@@ -30,7 +30,6 @@ function buildHTMLResultsTable(url) {
                 <th id="minPlayersColumnHeader" onclick="sortTableByMinPlayers()">Min. Players</th>
                 <th id="maxPlayersColumnHeader" onclick="sortTableByMaxPlayers()">Max. Players</th>
                 <th id="synopsisColumnHeader">Synopsis</th>
-                <th id="addToCollectionHeader"></th>
             </tr>
         </thead>
         <tbody>
@@ -63,11 +62,11 @@ function buildHTMLResultsTable(url) {
                     <img class="poster" src="${game.thumb_url}"><br>
                 </th>
                 <th class="titleCell">
-                    <a id="movieTitle${i}" href="/movies/details/${movie.title}">${movie.title}</a><br><br>
+                    <a id="gameTitle${i}" href="${game.name}">${game.name}</a><br><br>
 
                     <!-- For demonstration purposes only currently -->
                     <button id="dropdown-button${i}" onclick="prepareDatabaseInformationForm(${i}); toggleAddToCollectionDropdownForm(${i});">Add to Collection</button>
-                    <p id="themoviedbApiId${i}" hidden>${movie.id}</p>
+                    <p id="boardGameAtlasApiId${i}" hidden>${game.id}</p>
                     <form id="userCollectionDropdown${i}" style="display:none;"><hr>
                         <div id="selectDropdownDiv"></div>
                     <button type="button" onclick="addNewMovieToDatabase();">Submit</button>
@@ -75,20 +74,22 @@ function buildHTMLResultsTable(url) {
                     <!-- -->
                 </th>
                 <th class="yearCell">
-                    <p id="movieDate${i}">${movie.release_date}</p>
+                    <p id="gameDate${i}">${game.year_published}</p>
                 </th>
                 <th class="genre1Cell">
-                    <p id="primaryGenre${i}">${movie.genre_ids[0].toString().replace("28", "Action").replace("12", "Adventure").replace("16", "Animation").replace("35", "Comedy").replace("80", "Crime").replace("99", "Documentary").replace("18", "Drama").replace("10751", "Family").replace("14", "Fantasy").replace("36", "History").replace("27", "Horror").replace("10402", "Music").replace("9648", "Mystery").replace("10749", "Romance").replace("878", "Science Fiction").replace("10770", "TV Movie").replace("53", "Thriller").replace("10752", "War").replace("37", "Western")}</p>
-                    <p id="movieGenres${i}" hidden>${movie.genre_ids}</p>
+                    <p id="primaryGenre${i}">${game.categories[0]}</p>
+                    <p id="gameGenres${i}" hidden>${game.categories}</p>
+                </th>
+                <th class="minPlayersCell">
+                    <p id="gameMinPlayers${i}">${game.min_players}</p>
+                </th>
+                <th class="maxPlayersCell">
+                    <p id="gameMaxPlayers${i}">${game.max_players}</p>
                 </th>
                 <th class="synopsisCell">
-                    <p id="movieSynopsis${i}">${movie.overview}</p>
+                    <p id="gameSynopsis${i}">${game.description}</p>
+                </th>
 
-                </th>
-                <th class="streamingPlatformsCell">
-                    <button onclick="buildStreamingServicesHTMLDiv(themoviedbApiId${i}, streamingDiv${i}); toggleStreamingServicesDiv(streamingDiv${i})">Streaming Platforms</button>
-                    <div id="streamingDiv${i}" class="hidden"></div>
-                </th>
             </tr>
             `;
     }
