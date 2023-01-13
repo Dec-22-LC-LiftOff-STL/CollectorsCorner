32b7b45ae59002d16bbe51b14a37050f9b13dd4a
Connor Hellman <108197430+Connoreh@users.noreply.github.com>
2023-01-12 18:48:21 -0600

    Added an authentication filter, with two classes , AuthenticationFilter where we create the filter class and what we are preventing it to view, and WebApplicationConfig where we actually register that filter with spring. Had an issue with @Controller on AuthenticationFilter and the field authenticationFilter in WebAppConfig, spring didn't like managing two variables with the same name. Can add an override in the properties section, which I added but as a comment, but instead I just took Controller out of the authenticationFilter class.

---
+++ src/main/java/com/collectorscorner/demo/AuthenticationFilter.java
@@ -0,0 +1,65 @@
+package com.collectorscorner.demo;
+
+import com.collectorscorner.demo.controllers.AuthenticationController;
+import com.collectorscorner.demo.data.UserRepository;
+import com.collectorscorner.demo.models.User;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+
+//@Controller
+public class AuthenticationFilter implements HandlerInterceptor {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private AuthenticationController authenticationController;
+
+    private static final List<String> whitelist = Arrays.asList("/login", "/register", "/logout", "/css");
+
+
+    // Override prehandle method to see if a user is logged in and in a session before allowing get/post requests
+    @Override
+    public boolean preHandle(HttpServletRequest request,
+                             HttpServletResponse response,
+                             Object handler
+    ) throws IOException {
+// is the uri part of the whitelist?
+        if(isWhitelisted(request.getRequestURI())){
+            return true;
+        }
+        HttpSession session = request.getSession();
+
+        User user = authenticationController.getUserFromSession(session);
+
+        if (user != null){
+            return true;
+        }
+
+        response.sendRedirect("/login");
+        return false;
+    }
+
+
+    //Check if the address Starts with /pathRoot
+    private static boolean isWhitelisted(String path) {
+        for (String pathRoot : whitelist) {
+            if (path.startsWith(pathRoot)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+
+
+
+}
