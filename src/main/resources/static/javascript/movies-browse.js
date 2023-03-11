259397aa1c3664428c47a7d9c21043bc7da5c107
Nick Hoefle <Nickhoefle@gmail.com>
2023-03-11 16:39:51 -0600

    slider working for cast when images for actors exist.

--- src/main/resources/static/javascript/movies-browse.js
+++ src/main/resources/static/javascript/movies-browse.js
@@ -38,7 +38,7 @@ function buildHTMLResultsTable(url) {
                         <p id="movieImageURL${i}" hidden> ${'https://image.tmdb.org/t/p/w500' + movie.poster_path}</p><br>
                     </div>
                     <div class="infoColumn">
-                        <a id="movieTitle${i}" class="movieTitle" href="/movies/details/${movie.title}">${movie.title}</a><br>
+                        <a id="movieTitle${i}" class="movieTitle" href="/movies/details/${movie.title}&primary_release_year=${movie.release_date}">${movie.title}</a><br>
                         <p id="movieDate${i}" class="movieDate">${movie.release_date}</p>
                         <p id="primaryGenre${i}" class="primaryGenre">${movie.genre_ids[0].toString().replace("28", "Action").replace("12", "Adventure").replace("16", "Animation").replace("35", "Comedy").replace("80", "Crime").replace("99", "Documentary").replace("18", "Drama").replace("10751", "Family").replace("14", "Fantasy").replace("36", "History").replace("27", "Horror").replace("10402", "Music").replace("9648", "Mystery").replace("10749", "Romance").replace("878", "Science Fiction").replace("10770", "TV Movie").replace("53", "Thriller").replace("10752", "War").replace("37", "Western")}</p>
                         <p id="movieGenres${i}" hidden>${movie.genre_ids}</p>
@@ -287,7 +287,7 @@ function genericActorFetch(url) {
                             <p id="movieImageURL${i}" hidden> ${'https://image.tmdb.org/t/p/w500' + movie.poster_path}</p><br>
                         </div>
                         <div class="infoColumn">
-                            <a id="movieTitle${i}" class="movieTitle" href="/movies/details/${movie.title}">${movie.title}</a><br>
+                            <a id="movieTitle${i}" class="movieTitle" href="/movies/details/${movie.title}&primary_release_year=${movie.release_date}">${movie.title}</a><br>
                             <p id="movieDate${i}" class="movieDate">${movie.release_date}</p>
                             <p id="primaryGenre${i}" class="primaryGenre">${movie.genre_ids[0].toString().replace("28", "Action").replace("12", "Adventure").replace("16", "Animation").replace("35", "Comedy").replace("80", "Crime").replace("99", "Documentary").replace("18", "Drama").replace("10751", "Family").replace("14", "Fantasy").replace("36", "History").replace("27", "Horror").replace("10402", "Music").replace("9648", "Mystery").replace("10749", "Romance").replace("878", "Science Fiction").replace("10770", "TV Movie").replace("53", "Thriller").replace("10752", "War").replace("37", "Western")}</p>
                             <p id="movieGenres${i}" hidden>${movie.genre_ids}</p>
