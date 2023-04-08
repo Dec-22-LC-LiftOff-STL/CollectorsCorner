3224e50f7e052db777168da8256de73fb225802a
Nick <nickhoefle@gmail.com>
2023-04-07 23:02:02 -0500

    refactored sort function to account for and sort based on surnames.

--- src/main/resources/static/javascript/collections-delete.js
+++ src/main/resources/static/javascript/collections-delete.js
@@ -1,22 +1,41 @@
 function sortTable(table, column) {
-    var table = document.getElementById(table);
-    var rows = Array.from(table.tBodies[0].rows);
-    var sortOrder = table.getAttribute('data-sort-order') || 'asc';
+    var validTables = ["moviesTable", "booksTable"];
+    if (!validTables.includes(table)) {
+        console.error("Invalid table name. Expected 'moviesTable' or 'booksTable'.");
+        return;
+    }
+
+    var tableElement = document.getElementById(table);
+    var rows = Array.from(tableElement.tBodies[0].rows);
+    var sortOrder = tableElement.getAttribute('data-sort-order') || 'asc';
     var sortDirection = sortOrder === 'asc' ? 1 : -1;
 
     rows.sort(function(rowA, rowB) {
         var cellA = rowA.cells[column].textContent.trim().toLowerCase();
         var cellB = rowB.cells[column].textContent.trim().toLowerCase();
 
-        if (cellA === cellB) {
-            return 0;
-        }
+        // Extract last word (surname) from cell value if table is 'moviesTable' or 'booksTable'
+        if (table === 'moviesTable' || table === 'booksTable') {
+            var surnameA = cellA.split(' ').pop();
+            var surnameB = cellB.split(' ').pop();
 
-    return cellA < cellB ? -1 * sortDirection : sortDirection;
+            if (surnameA === surnameB) {
+                return 0;
+            }
+
+            return surnameA < surnameB ? -1 * sortDirection : sortDirection;
+        } else {
+            // Default sorting logic for other tables
+            if (cellA === cellB) {
+                return 0;
+            }
+
+            return cellA < cellB ? -1 * sortDirection : sortDirection;
+        }
     });
 
-    table.tBodies[0].append(...rows);
+    tableElement.tBodies[0].append(...rows);
 
     sortOrder = sortOrder === 'asc' ? 'desc' : 'asc';
-    table.setAttribute('data-sort-order', sortOrder);
+    tableElement.setAttribute('data-sort-order', sortOrder);
 }
