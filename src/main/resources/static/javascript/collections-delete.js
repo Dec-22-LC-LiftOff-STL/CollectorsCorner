e9723fe1f297c046a5ea49ebf84ae19b177bd26c
Nick <nickhoefle@gmail.com>
2023-04-07 23:40:56 -0500

    dark mode colors, alignment/spacing, formatting, and sorting working for collections/delete/games.

--- src/main/resources/static/javascript/collections-delete.js
+++ src/main/resources/static/javascript/collections-delete.js
@@ -1,41 +1,31 @@
-function sortTable(table, column) {
-    var validTables = ["moviesTable", "booksTable"];
-    if (!validTables.includes(table)) {
-        console.error("Invalid table name. Expected 'moviesTable' or 'booksTable'.");
-        return;
-    }
-
-    var tableElement = document.getElementById(table);
-    var rows = Array.from(tableElement.tBodies[0].rows);
-    var sortOrder = tableElement.getAttribute('data-sort-order') || 'asc';
+function sortTable(tableId, column) {
+    var table = document.getElementById(tableId);
+    var rows = Array.from(table.tBodies[0].rows);
+    var sortOrder = table.getAttribute('data-sort-order') || 'asc';
     var sortDirection = sortOrder === 'asc' ? 1 : -1;
 
     rows.sort(function(rowA, rowB) {
         var cellA = rowA.cells[column].textContent.trim().toLowerCase();
         var cellB = rowB.cells[column].textContent.trim().toLowerCase();
 
-        // Extract last word (surname) from cell value if table is 'moviesTable' or 'booksTable'
-        if (table === 'moviesTable' || table === 'booksTable') {
-            var surnameA = cellA.split(' ').pop();
-            var surnameB = cellB.split(' ').pop();
+        if (tableId !== 'gamesTable' && column === 3) {
+            // Extract the last word (surname) for other tables and column 3
+            var lastWordA = cellA.split(' ').pop();
+            var lastWordB = cellB.split(' ').pop();
 
-            if (surnameA === surnameB) {
-                return 0;
-            }
-
-            return surnameA < surnameB ? -1 * sortDirection : sortDirection;
-        } else {
-            // Default sorting logic for other tables
-            if (cellA === cellB) {
-                return 0;
-            }
-
-            return cellA < cellB ? -1 * sortDirection : sortDirection;
+            cellA = lastWordA;
+            cellB = lastWordB;
         }
+
+        if (cellA === cellB) {
+            return 0;
+        }
+
+        return cellA < cellB ? -1 * sortDirection : sortDirection;
     });
 
-    tableElement.tBodies[0].append(...rows);
+    table.tBodies[0].append(...rows);
 
     sortOrder = sortOrder === 'asc' ? 'desc' : 'asc';
-    tableElement.setAttribute('data-sort-order', sortOrder);
+    table.setAttribute('data-sort-order', sortOrder);
 }
